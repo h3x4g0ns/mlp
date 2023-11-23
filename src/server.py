9f3a436de633c152b546bbf7f5d08747b06730ee
h3x4g0ns <agrawalekansh02@gmail.com>
2023-11-22 16:32:34 -0800

    basic argparser

--- src/server.py
+++ src/server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 import cv2
 import numpy as np
-import os
+import argparse
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -10,15 +10,15 @@ socketio = SocketIO(app)
 # Dummy ML model function
 def process_frame(frame):
     # Process the frame with your ML model here
-    # For demonstration, let's just convert it to grayscale
+    # For demonstration, let"s just convert it to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     return gray
 
-@app.route('/')
+@app.route("/")
 def index():
     return "<h1>hello there</h1>" 
 
-@socketio.on('frame')
+@socketio.on("frame")
 def handle_frame(data):
     # Decode the frame
     pimg = np.frombuffer(data, dtype=np.uint8)
@@ -28,8 +28,8 @@ def handle_frame(data):
     processed_frame = process_frame(frame)
 
     # Send back the processed frame
-    _, buffer = cv2.imencode('.jpg', processed_frame)
-    socketio.emit('response', buffer)
+    _, buffer = cv2.imencode(".jpg", processed_frame)
+    socketio.emit("response", buffer)
 
 @socketio.on("hello-world")
 def handle_hello(data):
@@ -37,6 +37,11 @@ def handle_hello(data):
     encoded_message = str.encode(message)
     socketio.emit("response", encoded_message)
 
-if __name__ == '__main__':
-    debug = os.environ.get("DEBUG", None)
-    socketio.run(app=app, debug=debug, port=5050)
+def main(params):
+    socketio.run(app=app, debug=params.debug, host=params.host, port=params.port)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="MLP: socket inference server for open-ended ML models")
+    parser.add_argument("--host", default="0.0.0.0", help="Host IP address")
+    parser.add_argument("--port", type=int, default=5050, help="Port number")
+    parser.add_argument("--debug", action="store_true", default=True, help="Debug mode")
