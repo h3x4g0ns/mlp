0dd61cc58066786f565c4196dab5e547991b0759
h3x4g0ns <agrawalekansh02@gmail.com>
2024-03-13 16:08:27 -0700

    basic server bringup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mlp
+
 lightweight ML inference server with sockets
+
+
+## Getting Started
+
+Best way to run server is to run the `docker-compose.yml` file:
+
+```sh
+docker-compose -f build/Dockerfile up --build
+```
--- src/server.py
+++ src/server.py
@@ -1,41 +1,53 @@
 from flask import Flask
 from flask_socketio import SocketIO
-import cv2
-import numpy as np
 import argparse
+
+from .utils import base64_to_img, img_to_base64, process_frame
+
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# Dummy ML model function
-def process_frame(frame):
-    # Process the frame with your ML model here
-    # For demonstration, let"s just convert it to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    return gray
 
 @app.route("/")
 def index():
     return "<h1>hello there</h1>" 
 
-@socketio.on("frame")
-def handle_frame(data):
-    # Decode the frame
-    pimg = np.frombuffer(data, dtype=np.uint8)
-    frame = cv2.imdecode(pimg, 1)
-
-    # Process the frame
+@socketio.on("grayscale")
+def handle_grayscale(data):
+    """
+    Inference function to process gray the frame and send the result back
+    Image is passed in as a base64 encoded string
+    """
+    frame = base64_to_img(data)
     processed_frame = process_frame(frame)
+    encoded_str = img_to_base64(processed_frame)
+    socketio.emit("gray-response", encoded_str)
 
-    # Send back the processed frame
-    _, buffer = cv2.imencode(".jpg", processed_frame)
-    socketio.emit("response", buffer)
+
+@socketio.on("segment")
+def handle_segmentation(data):
+    """
+    Inference function to process the frame and send the result back
+    Image is passed in as a base64 encoded string
+    """
+    from ..process import segment
+
+    frame = base64_to_img(data)
+    processed_frame = segment(frame)
+    encoded_str = img_to_base64(processed_frame)
+    socketio.emit("segment-response", encoded_str)
+
 
 @socketio.on("hello-world")
 def handle_hello(data):
+    """
+    Simple hello world function to test the connection
+    """
     message = "hello world"
     encoded_message = str.encode(message)
     socketio.emit("response", encoded_message)
+
 
 def main(params):
     socketio.run(app=app, debug=params.debug, host=params.host, port=params.port)
@@ -45,3 +57,5 @@ if __name__ == "__main__":
     parser.add_argument("--host", default="0.0.0.0", help="Host IP address")
     parser.add_argument("--port", type=int, default=5050, help="Port number")
     parser.add_argument("--debug", action="store_true", default=True, help="Debug mode")
+    args = parser.parse_args()
+    main(args)
